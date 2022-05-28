f22f7cab17be14fe496b16a0d19c5b33eccba53a
Hao Sitong <hst1142007403@gmail.com>
2022-05-29 07:39:06 +0800

    first commit

---
+++ lib/server_web/channels/room_channel.ex
@@ -0,0 +1,42 @@
+defmodule ServerWeb.RoomChannel do
+  use ServerWeb, :channel
+
+
+  @impl true
+  @spec join(<<_::80>>, any, any) :: {:ok, any}
+  def join("room:" <> room_id, payload, socket) do
+    if authorized?(payload) do
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  @impl true
+  @spec handle_in(<<_::32, _::_*8>>, any, any) ::
+          {:noreply, Phoenix.Socket.t()} | {:reply, {:ok, any}, any}
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (room:lobby).
+  @impl true
+  def handle_in("joined", payload, socket) do
+    broadcast(socket, "joined", payload)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_in("new_guess", payload, socket) do
+    broadcast(socket, "new_guess", payload)
+    {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+end
