d7f111372720f4ee09be0d795f70b2cddfaf4b5e
Hao Sitong <hst1142007403@gmail.com>
2022-07-16 22:15:03 +0800

    cleanup

--- lib/server_web/channels/room_channel.ex
+++ lib/server_web/channels/room_channel.ex
@@ -1,31 +1,14 @@
 defmodule ServerWeb.RoomChannel do
   use ServerWeb, :channel
-  alias ServerWeb.Presence
-
-  # def join("room:new", _payload, socket) do
-  #   Server.Datastore.createRoom()
-  #   {:ok, :ets.lookup(:rooms, :ets.first(:rooms)) |> List.first |> (fn({n, a, f}) -> %{room_id: n, players: a |> Tuple.to_list(), answer: f} end).(),socket}
-  # end
 
   @impl true
   def join("room:" <> roomId, payload, socket) do
     {status, _} = Server.Datastore.joinRoom(roomId, socket.assigns.playerId, payload["playerName"])
-    send(self(), :after_join)
-    IO.inspect(payload["playerName"])
     if status == :ok do
       {:ok, assign(socket, :playerName, payload["playerName"])}
     else
       {:error, %{reason: "invalid room id"}}
     end
-    # :ets.insert(:rooms, {room_id, {0,1}, "hello"})
-    # {:ok, :ets.lookup(:rooms, room_id) |> List.first |> (fn({n, a, f}) -> %{room_id: n, players: a |> Tuple.to_list(), answer: f} end).(),socket}
-  end
-
-  @impl true
-  def handle_info(:after_join, socket) do
-    {:ok, _} = Presence.track(socket, socket.assigns.playerId, %{})
-    push(socket, "presence_state", Presence.list(socket))
-    {:noreply, socket}
   end
 
   # Channels can be used in a request/response fashion
@@ -61,11 +44,39 @@ defmodule ServerWeb.RoomChannel do
     {:noreply, socket}
   end
 
-  defp startRound(socket) do
+  @impl true
+  def handle_in("new_guess", payload, socket) do
+    "room:" <> roomId = socket.topic
+
+    {:ok, %{result: result}} = Server.Datastore.evaluateGuess(roomId, socket.assigns.playerId, payload["guess"])
+
+    {:ok, pData} = Server.Datastore.getPlayer(socket.assigns.playerId)
+
+    row = pData[:row]
+    broadcast(socket, "new_guess",  %{playerId: socket.assigns.playerId, playerName: socket.assigns.playerName, row: row})
+
+    if result === "22222" do #correct
+      {:ok, %{allFinished: allFinished}} = Server.Datastore.finish(socket.assigns.playerId, roomId, true)
+      broadcast(socket, "finish", %{playerId: socket.assigns.playerId, playerName: socket.assigns.playerName, result: "correct"})
+      if allFinished, do: endRound(roomId, socket)
+      {:reply, {:ok, %{result: result, allFinished: allFinished}}, socket}
+    else
+      if pData[:row] >= 6 do #run out of guesses
+      {:ok, %{allFinished: allFinished}} = Server.Datastore.finish(socket.assigns.playerId, roomId, false)
+      broadcast(socket, "finish", %{playerId: socket.assigns.playerId, playerName: socket.assigns.playerName, result: "wrong"})
+      if allFinished, do: endRound(roomId, socket)
+      {:reply, {:ok, %{result: result, allFinished: allFinished, row: row}}, socket}
+      else
+        {:reply, {:ok, %{result: result, allFinished: false, row: row}}, socket}
+      end
+    end
+  end
+
+    defp startRound(socket) do
     "room:" <> roomId = socket.topic
     {:ok, %{roomId: _, players: _, started: _, round: round} } = Server.Datastore.getRoom(roomId);
     Task.start(fn ->
-        :timer.sleep(180000) #180000
+        :timer.sleep(180000)
         timerEndRound(roomId, socket, round)
       end)
     broadcast(socket, "start_round", %{})
@@ -92,68 +103,4 @@ defmodule ServerWeb.RoomChannel do
       end
     end
   end
-
-  @impl true
-  def handle_in("new_guess", payload, socket) do
-    # IO.inspect(:ets.lookup(:rooms, :ets.first(:rooms)))
-    "room:" <> roomId = socket.topic
-
-    {status, %{result: result}} = Server.Datastore.evaluateGuess(roomId, socket.assigns.playerId, payload["guess"])
-
-    if status === :ok do
-      {:ok, pData} = Server.Datastore.getPlayer(socket.assigns.playerId)
-
-      row = pData[:row]
-      broadcast(socket, "new_guess",  %{playerId: socket.assigns.playerId, playerName: socket.assigns.playerName, row: row})
-
-      if result === "22222" do #correct
-        {:ok, %{allFinished: allFinished}} = Server.Datastore.finish(socket.assigns.playerId, roomId, true)
-        broadcast(socket, "finish", %{playerId: socket.assigns.playerId, playerName: socket.assigns.playerName, result: "correct"})
-        if allFinished, do: endRound(roomId, socket)
-        {:reply, {:ok, %{result: result, allFinished: allFinished}}, socket}
-      else
-        if pData[:row] >= 6 do #run out of guesses
-        {:ok, %{allFinished: allFinished}} = Server.Datastore.finish(socket.assigns.playerId, roomId, false)
-        broadcast(socket, "finish", %{playerId: socket.assigns.playerId, playerName: socket.assigns.playerName, result: "wrong"})
-        if allFinished, do: endRound(roomId, socket)
-        {:reply, {:ok, %{result: result, allFinished: allFinished, row: row}}, socket}
-        else
-          {:reply, {:ok, %{result: result, allFinished: false, row: row}}, socket}
-        end
-      end
-    else
-      {:noreply, socket}
-    end
-  end
-
-  # intercept ["start_round"]
-
-  # def handle_out("start_round", _msg, socket) do
-  #   "room:" <> roomId = socket.topic
-  #   Task.start(fn ->
-  #       :timer.sleep(10000) #180000
-  #       {:ok, %{roomId: _, players: _, started: _, round: round} } = Server.Datastore.getRoom(roomId);
-  #       IO.write("timerEndRound, roundToEnd is: ")
-  #       IO.inspect(round)
-  #       timerEndRound(roomId, socket, round)
-  #     end)
-  #   {:noreply, socket}
-  # end
-
-  # Phoenix.Channel.intercept(["presence_diff"])
-
-  # @impl true
-  # def handle_out("presence_diff", payload, socket) do
-  #   %{:joins => joins, :leaves => leaves} = payload
-  #   IO.write("presence diff ")
-  #   IO.inspect(joins)
-  #   "room:" <> roomId = socket.topic
-  #   for _z <- joins, do: Server.Datastore.updateCounter(roomId, true)
-  #   for _z <- leaves, do: Server.Datastore.updateCounter(roomId, false)
-  # end
-
-  # Add authorization logic here as required.
-  # defp authorized?(_payload) do
-  #   true
-  # end
 end
